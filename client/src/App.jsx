bce6bdec301e97287a2c780cc82d735492fd8799
Vaibhav Mahajan <vaibhavmahajan3003@gmail.com>
2023-07-30 14:47:12 +0530

    frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,28 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import {createTheme} from '@mui/material/styles';
+import { themeSettings } from "./theme";
+
 
 function App() {
 
+  const mode = useSelector((state)=>state.mode);
+  const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode]);
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/profile/:userId" element={<Profile/>}/>
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </ThemeProvider>
     </Router>
   );
 }
